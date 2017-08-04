acd805f3a85eb89f007b07f100f20b7545004aad
ArtNerd10 <kagerapeter6@gmail.com>
2017-08-04 13:37:11 +0300

    My Fourth commit

---
+++ app/src/main/java/com/example/ghostofart/spinthebottle/BottleActivity.java
@@ -0,0 +1,100 @@
+package com.example.ghostofart.spinthebottle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class BottleActivity extends AppCompatActivity {
+
+    ImageView tusker4;
+
+    Button b_go;
+
+    Random r;
+
+    int angle;
+
+    boolean restart = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottle);
+
+        r = new Random();
+
+        tusker4 = (ImageView) findViewById(R.id.tusker4);
+
+        b_go = (Button) findViewById(R.id.b_go);
+
+        b_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (restart)
+                {
+                    angle = angle % 360;
+                    angle = r.nextInt() + 360;
+                    RotateAnimation rotate = new RotateAnimation(angle, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setFillAfter(true);
+                    rotate.setDuration(1000);
+                    rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+
+                    tusker4.startAnimation(rotate);
+
+                    b_go.setText("SPIN AGAIN");
+
+                    restart = false;
+                }
+                else
+                {
+                    angle = r.nextInt(3600) + 360;
+                    RotateAnimation rotate = new RotateAnimation(0, angle, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setFillAfter(true);
+                    rotate.setDuration(3600);
+                    rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+
+                    tusker4.startAnimation(rotate);
+
+
+
+                    restart = true;
+
+                    b_go.setText("RESET BOTTLE");
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bottle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
